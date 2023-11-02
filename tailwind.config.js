18a50b4f24a4ab43e88a4d7fe000607ddf3397b5
Daniel Plundrich <dan.plundrich@gmail.com>
2023-11-02 20:35:08 +0100

    vytvořena struktura a responzivní zobrazení

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        BrightRed: '#f25f3a',
+        DarkBlue: '#242d52',
+      }
+    },
+
+  },
+
+  plugins: [],
+
+
+
+}
